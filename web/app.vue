f49be9aef7b167aaf7fbbca8069287510e453c17
Ava's DAO Contributors <support@avasdao.org>
2022-12-07 16:23:43 -0500

    Update web homepage.

--- web/app.vue
+++ web/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <main>
+        <h1 class="text-3xl font-medium bg-pink-500">
+            Nexa Python Library
+        </h1>
+    </main>
 </template>
